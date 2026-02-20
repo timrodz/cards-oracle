8ffb5603957dd75eeea8020a6fdc4c1ebef0e2d9
Juan Morais <timrodz@icloud.com>
2026-02-20 13:33:14 +1300

    refactor: FastAPI lifespan events for shutdown

--- apps/backend/app/main.py
+++ apps/backend/app/main.py
@@ -1,4 +1,5 @@
 import sys
+from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
@@ -6,6 +7,7 @@ from loguru import logger
 
 from app.api.main import api_router
 from app.core.config import app_settings
+from app.core.embeddings_jobs import embeddings_job_service
 from app.models.api import HealthCheckResponse
 
 app = FastAPI(title="Card Oracle")
@@ -32,6 +34,13 @@ if origins:
 @app.get("/", response_model=HealthCheckResponse)
 def health_check() -> HealthCheckResponse:
     return HealthCheckResponse(Hello="World")
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # TODO: Load general instances here
+    yield
+    embeddings_job_service.shutdown()
 
 
 app.include_router(api_router)
